6f5043268ede39335b912fc1241d87e49f00bf4b
Mark Melnic <markhappy200@gmail.com>
2020-10-28 10:32:02 +0100

    Switched to a path handler

--- exec.py
+++ exec.py
@@ -3,24 +3,11 @@ from argparse import ArgumentParser, RawTextHelpFormatter
 if __name__ == "__main__":
     parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
     parser.add_argument(
-        "module",
-        metavar="Py assignment module",
+        "path",
+        metavar="path",
         type=str,
-        help="What module is the file located in.",
-    )
-    parser.add_argument(
-        "folder",
-        metavar="Py file folder",
-        type=str,
-        help="What folder is the file located in.",
-    )
-    parser.add_argument(
-        "filename",
-        metavar="Py filename",
-        type=str,
-        help="What file would you like to run.",
+        help="Path of the file to be executed.",
     )
     args = parser.parse_args()
 
-    file_path = args.module + "/" + args.folder + "/" + args.filename
-    exec(compile(open(file_path, "rb").read(), file_path, "exec"))
+    exec(compile(open(args.path, "rb").read(), args.path, "exec"))
