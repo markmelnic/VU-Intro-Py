5c1021f686c68c793c65dcd2d1e4af0cfc5a454f
Mark Melnic <markhappy200@gmail.com>
2020-10-28 10:39:49 +0100

    Added assignment NPP

---
+++ Module 3/Nuclear Power Plant/nuclear_power_plant-mmc560.py
@@ -0,0 +1,18 @@
+""" Assignment: nuclear power plant
+    Created on 28 oct. 2020
+    @author: Mark Melnic """
+
+
+def message():
+    print(
+        """
+        NUCLEAR CORE UNSTABLE!!!
+        Quarantine is in effect.
+        Surrounding hamlets will be evacuated.
+        Anti-radiationsuits and iodine pills are mandatory.
+        """
+    )
+
+
+for i in range(3):
+    message()
