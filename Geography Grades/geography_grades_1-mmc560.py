d8664f9634be71e4be21d678cbb6096ad531d2cd
Mark Melnic <markhappy200@gmail.com>
2020-11-10 17:28:50 +0100

    Added a new implementation

--- Geography Grades/geography_grades_1-mmc560.py
+++ Geography Grades/geography_grades_1-mmc560.py
@@ -2,29 +2,24 @@
     Created on 9 nov. 2020
     @author: Mark Melnic """
 
-with open("input.txt", "r") as input_file:
+def process_student(line: str):
+    split_line = line.split("_")
+    name = split_line[0]
+    grades = split_line[-1]
+
+    grades = grades.split(" ")
+    grades = [float(g) for g in grades]
+
+    average = sum(grades) / len(grades)
+
+    print("%s has an average grade of %.2f" % (name, average))
+
+
+with open("Geography Grades/input.txt", "r") as input_file:
     students = input_file.readlines()
 
-    print("Report for group 2b")
-    for student in students:
-        name = ""
-        for ch in student:
-            if not ch == "_":
-                name += ch
-            else:
-                break
+print("Report for group 2b")
+for student in students:
+    process_student(student)
 
-        grades = ""
-        for ch in student[::-1]:
-            if not ch == "_":
-                grades += ch
-            else:
-                break
-
-        grades = grades[::-1].split(" ")
-        grades = [float(g) for g in grades]
-        average = sum(grades) / len(grades)
-
-        print("%s has an average grade of %.2f" % (name, average))
-
-    print("End of report")
+print("End of report")
