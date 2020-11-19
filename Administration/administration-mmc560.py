20e4c0577f71481d376cc6c2b1262aecbba58cc4
Mark Melnic <markhappy200@gmail.com>
2020-11-19 13:13:28 +0100

    Final version Administration assignment

--- Administration/administration-mmc560.py
+++ Administration/administration-mmc560.py
@@ -1,25 +1,45 @@
 """ Assignment: administration
-    Created on 28 oct. 2020
+    Created on 17 nov. 2020
     @author: Mark Melnic """
 
 
 def name_grades(line):
-    name = ""
-    for ch in line:
-        if not ch == "_":
-            name += ch
-        else:
-            break
+    name, grades = list(filter(lambda item: item != "", line.split("_")))
 
-    grades = ""
-    for ch in line[::-1]:
-        if not ch == "_":
-            grades += ch
-        else:
-            break
+    grades = [float(g) for g in grades.split(" ")]
 
-    grades = [float(g) for g in grades[::-1].split(" ")]
+    average = get_average(grades)
 
+    print("%s has an average of %s" % (name, str(average)))
+
+
+def representation(line):
+    similarities, names = line.split(";")
+
+    graph = generate_graph(similarities)
+    print("    %s" % graph, end="")
+
+    names = names.split(",")
+    if len(names) == 0 or names[0] == "":
+        print("\n    No matches found", end="")
+    else:
+        for name in names:
+            print("\n   ", name, end="")
+
+
+def generate_graph(similarities):
+    graph = ""
+    for number in similarities.split("="):
+        if int(number) == 0:
+            graph += "_"
+        elif int(number) > 0 and int(number) < 20:
+            graph += "-"
+        elif int(number) >= 20:
+            graph += "^"
+    return graph
+
+
+def get_average(grades):
     average = sum(grades) / len(grades)
     if average >= 5.5 and average < 6:
         average = str(int(average) + 1) + "-"
@@ -31,39 +51,15 @@ def name_grades(line):
             average = int(average) + 0.5
         else:
             average = int(average) + 1
-
-    print("%s has an average of %s" % (name, str(average)))
-
-
-def representation(line):
-    similarities, names = line.split(";")
-
-    graph = ""
-    for num in similarities.split("="):
-        if int(num) == 0:
-            graph += "_"
-        elif int(num) > 0 and int(num) < 20:
-            graph += "-"
-        elif int(num) >= 20:
-            graph += "^"
-    print("    %s" % graph, end="")
-
-    names = names.split(",")
-    if len(names) == 0 or names[0] == "":
-        print("\n    No matches found", end="")
-    else:
-        for name in names:
-            print("\n   ", name, end="")
+    return str(average)
 
 
 if __name__ == "__main__":
-    filename = str(input("What is the file name (withouth the .txt extension): "))
+    filename = str(input("What is the file name (leave empty if \"input.txt\"): "))
     if filename == "":
         filename = "input.txt"
-    else:
-        filename += ".txt"
 
-    with open("Administration/" + filename, "r") as input_file:
+    with open(filename, "r") as input_file:
         lines = input_file.readlines()
 
     for i, line in enumerate(lines):
