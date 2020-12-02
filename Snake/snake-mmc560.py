84767f15b23c5e082e4eb7a31805f50800e50292
Mark Melnic <markhappy200@gmail.com>
2020-12-02 10:49:37 +0100

    Initiated snake

--- Snake/snake-mmc560.py
+++ Snake/snake-mmc560.py
@@ -2,6 +2,27 @@
     Created on 29 oct. 2020
     @author: Mark Melnic """
 
-from Snake.ipy_lib3 import SnakeUserInterface
+from ipy_lib3 import SnakeUserInterface
 
-it = SnakeUserInterface(32, 24)
+height = 32
+width = 24
+
+root = SnakeUserInterface(height, width)
+
+init_pos = [(0,0), (1,0)]
+food_pos = (0,0)
+
+while True:
+    root.clear()
+
+    while food_pos in init_pos:
+        food_pos = (root.random(height), root.random(width))
+    root.place(root.random(height), root.random(width), 1)
+
+    for pos in init_pos:
+        root.place(pos[0], pos[1], 2)
+
+    root.show()
+    root.wait(500)
+
+root.close()
