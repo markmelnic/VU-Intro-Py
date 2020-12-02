b306df669551c48d6db185dde87fc4c55a125418
Mark Melnic <markhappy200@gmail.com>
2020-12-02 18:31:28 +0100

    Game of life initiated

---
+++ Life/life-mmc560.py
@@ -0,0 +1,11 @@
+""" Assignment: Snake
+    Created on 2 dec. 2020
+    @author: Mark Melnic """
+
+from ipy_lib3 import LifeUserInterface
+
+WIDTH = 9
+HEIGHT = 9
+
+if __name__ == "__main__":
+    print()
