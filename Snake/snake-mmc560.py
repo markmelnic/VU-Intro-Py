427dc6ed76561038f1a0dd90c3927689465bea61
Mark Melnic <markhappy200@gmail.com>
2020-12-02 17:43:06 +0100

    Finished snake assignment

--- Snake/snake-mmc560.py
+++ Snake/snake-mmc560.py
@@ -1,28 +1,74 @@
 """ Assignment: Snake
-    Created on 29 oct. 2020
+    Created on 2 dec. 2020
     @author: Mark Melnic """
 
 from ipy_lib3 import SnakeUserInterface
 
-height = 32
-width = 24
+HEIGHT = 32
+WIDTH = 24
+snake_pos = [(0, 0), (1, 0)]
+food_pos = (0, 0)
+event_name = "arrow"
+event_data = "r"
 
-root = SnakeUserInterface(height, width)
+if __name__ == "__main__":
+    root = SnakeUserInterface(WIDTH, HEIGHT)
+    while food_pos in snake_pos:
+        food_pos = (root.random(WIDTH), root.random(HEIGHT))
 
-init_pos = [(0,0), (1,0)]
-food_pos = (0,0)
+    while True:
+        ev = root.get_event()
+        if ev.name == "arrow":
+            if (
+                (event_data == "r" and not ev.data == "l")
+                or (event_data == "l" and not ev.data == "r")
+                or (event_data == "u" and not ev.data == "d")
+                or (event_data == "d" and not ev.data == "u")
+            ):
+                event_name = ev.name
+                event_data = ev.data
 
-while True:
-    root.clear()
+        if event_name == "arrow":
+            if event_data == "r":
+                new_piece = (snake_pos[-1][0] + 1, snake_pos[-1][1])
+            elif event_data == "l":
+                new_piece = (snake_pos[-1][0] - 1, snake_pos[-1][1])
+            elif event_data == "u":
+                new_piece = (snake_pos[-1][0], snake_pos[-1][1] - 1)
+            elif event_data == "d":
+                new_piece = (snake_pos[-1][0], snake_pos[-1][1] + 1)
 
-    while food_pos in init_pos:
-        food_pos = (root.random(height), root.random(width))
-    root.place(root.random(height), root.random(width), 1)
+            if new_piece in snake_pos:
+                root.print_("Game over")
+                break
+            else:
+                snake_pos.append(new_piece)
+                for i, piece in enumerate(snake_pos):
+                    piece = list(piece)
+                    if piece[0] == -1:
+                        piece[0] = WIDTH - 1
+                        snake_pos[i] = tuple(piece)
+                    elif piece[0] == WIDTH:
+                        piece[0] = 0
+                        snake_pos[i] = tuple(piece)
+                    if piece[1] == -1:
+                        piece[1] = HEIGHT - 1
+                        snake_pos[i] = tuple(piece)
+                    elif piece[1] == HEIGHT:
+                        piece[1] = 0
+                        snake_pos[i] = tuple(piece)
+                if snake_pos[-1] == food_pos:
+                    while food_pos in snake_pos:
+                        food_pos = (root.random(WIDTH), root.random(HEIGHT))
+                else:
+                    snake_pos.pop(0)
 
-    for pos in init_pos:
-        root.place(pos[0], pos[1], 2)
+        root.clear()
+        root.place(food_pos[0], food_pos[1], 1)
+        for pos in snake_pos:
+            root.place(pos[0], pos[1], 2)
+        root.show()
+        # root.wait(500)
 
-    root.show()
-    root.wait(500)
-
-root.close()
+    root.stay_open()
+    root.close()
