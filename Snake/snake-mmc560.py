85a4912dff53d7cf6f1edd6e57044b9d79f9f315
Mark Melnic <markhappy200@gmail.com>
2020-12-04 13:52:42 +0100

    Added input function and minor restructuration

--- Snake/snake-mmc560.py
+++ Snake/snake-mmc560.py
@@ -6,38 +6,43 @@ from ipy_lib3 import SnakeUserInterface
 
 WIDTH = 32
 HEIGHT = 24
-event_name = "arrow"
-walls = []
 
-filename = str(
-    input(
-        "Do you have a level to load? Type the file name here, otherwise just press ENTER: "
-    )
-)
-if not filename == "":
-    with open(filename, "r") as level:
-        content = level.read().split("=")
-        event_data = content[1].lower()
+def read_level():
+    walls = []
+    filename = str(input(
+            "Do you have a level to load? Type the file name here, otherwise just press ENTER: "))
+    if not filename == "":
+        with open(filename, "r") as level:
+            content = level.read().split("=")
+            event_data = content[1].lower()
 
-        snake_pos = []
-        for pos in content[0][::-1].split("\n"):
-            pos = pos.split()
-            snake_pos.append((int(pos[0]), int(pos[1])))
+            snake_pos = []
+            for pos in content[0][::-1].split("\n"):
+                pos = pos.split()
+                snake_pos.append((int(pos[0]), int(pos[1])))
 
-        for pos in content[2][:-1].split("\n"):
-            pos = pos.split()
-            walls.append((int(pos[0]), int(pos[1])))
+            for pos in content[2][:-1].split("\n"):
+                pos = pos.split()
+                walls.append((int(pos[0]), int(pos[1])))
 
-else:
-    snake_pos = [(0, 0), (1, 0)]
-    event_data = "r"
-food_pos = snake_pos[0]
+    else:
+        snake_pos = [(0, 0), (1, 0)]
+        event_data = "r"
+    return snake_pos, event_data, walls
 
-if __name__ == "__main__":
-    root = SnakeUserInterface(WIDTH, HEIGHT)
+def generate_food():
+    food_pos = (root.random(WIDTH), root.random(HEIGHT))
     while food_pos in snake_pos or food_pos in walls:
         food_pos = (root.random(WIDTH), root.random(HEIGHT))
+    return food_pos
 
+if __name__ == "__main__":
+    event_name = "arrow"
+    snake_pos, event_data, walls = read_level()
+    food_pos = snake_pos[0]
+
+    root = SnakeUserInterface(WIDTH, HEIGHT)
+    food_pos = generate_food()
     while True:
         root.clear()
         root.place(food_pos[0], food_pos[1], 1)
@@ -50,10 +55,10 @@ if __name__ == "__main__":
         ev = root.get_event()
         if ev.name == "arrow":
             if (
-                (event_data == "r" and not ev.data == "l")
-                or (event_data == "l" and not ev.data == "r")
-                or (event_data == "u" and not ev.data == "d")
-                or (event_data == "d" and not ev.data == "u")
+                (event_data == "r" and not ev.data == "l") or
+                (event_data == "l" and not ev.data == "r") or
+                (event_data == "u" and not ev.data == "d") or
+                (event_data == "d" and not ev.data == "u")
             ):
                 event_name = ev.name
                 event_data = ev.data
@@ -73,8 +78,8 @@ if __name__ == "__main__":
                 new_piece = (snake_pos[-1][0], snake_pos[-1][1] - 1)
             elif event_data == "d":
                 new_piece = (snake_pos[-1][0], snake_pos[-1][1] + 1)
-
             snake_pos.append(new_piece)
+
             if len(snake_pos) != len(set(snake_pos)) or any(pos in walls for pos in snake_pos):
                 root.clear_text()
                 root.print_("Game over")
@@ -95,8 +100,7 @@ if __name__ == "__main__":
                         piece[1] = 0
                         snake_pos[i] = tuple(piece)
                 if snake_pos[-1] == food_pos:
-                    while food_pos in snake_pos or food_pos in walls:
-                        food_pos = (root.random(WIDTH), root.random(HEIGHT))
+                    food_pos = generate_food()
                 else:
                     snake_pos.pop(0)
 
